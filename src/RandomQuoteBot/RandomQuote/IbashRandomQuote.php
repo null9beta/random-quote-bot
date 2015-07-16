1862a22541a3f8352c835a2bcc52be0bd7836546
Alberto Assmann <alberto.assmann@spryker.com>
2015-07-16 18:35:49 +0200

    - add quote class for ibash

---
+++ src/RandomQuoteBot/RandomQuote/IbashRandomQuote.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace RandomQuoteBot\RandomQuote;
+
+use DOMDocument;
+use DOMNode;
+use DOMNodeList;
+use RandomQuoteBot\AbstractRandomQuote;
+
+class IbashRandomQuote extends AbstractRandomQuote
+{
+    /**
+     * @throws \Exception
+     * @return string
+     */
+    protected function getRandomQuote()
+    {
+        $content = file_get_contents('http://www.ibash.de/random.html');
+        libxml_use_internal_errors(true);
+        $document = new DOMDocument();
+        $document->loadHtml($content);
+
+        $xpath = new \DOMXPath($document);
+
+        $quotes = $xpath->query('//td[@class="quote"]');
+
+        for ($i = 0; $i < 10; $i++) {
+            $randQuote = $this->getRandomEntry($quotes);
+
+            return $this->formatQuote($randQuote, $xpath);
+        }
+
+        throw new \Exception('Was not able to find a quote on iBash');
+    }
+
+    /**
+     * @param DOMNodeList $quotes
+     *
+     * @return mixed
+     */
+    protected function getRandomEntry(DOMNodeList $quotes)
+    {
+        $quoteNumber = mt_rand(0, $quotes->length);
+        $randQuote = $quotes->item($quoteNumber);
+        return $randQuote;
+    }
+
+    /**
+     * @param $randQuote
+     *
+     * @return string
+     */
+    protected function formatQuote($randQuote)
+    {
+
+        $line = trim($randQuote->textContent);
+        $line = preg_replace('/</', "\n *<", $line);
+        $line = preg_replace('/>/', '>*', $line);
+
+        return $line;
+    }
+}
+ 
